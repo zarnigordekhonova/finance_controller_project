d85ff9f8976e2570870de265980770934190d2e2
zarnigordekhonova <zarnigor1008@gmail.com>
2024-07-24 16:58:46 +0500

    all done

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'Users.CustomUser'
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
